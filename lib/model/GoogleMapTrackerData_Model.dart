5351781c36debee0958ef3f04f232a77578076c9
Praveen <balapraveen.meark@gmail.com>
2023-02-23 19:07:56 +0530

    Move Marker

---
+++ lib/model/GoogleMapTrackerData_Model.dart
@@ -0,0 +1,150 @@
+// To parse this JSON data, do
+//
+//     final googleMapVeicleTracking = googleMapVeicleTrackingFromJson(jsonString);
+
+import 'dart:convert';
+
+GoogleMapVeicleTracking googleMapVeicleTrackingFromJson(String str) => GoogleMapVeicleTracking.fromJson(json.decode(str));
+
+String googleMapVeicleTrackingToJson(GoogleMapVeicleTracking data) => json.encode(data.toJson());
+
+class GoogleMapVeicleTracking {
+    GoogleMapVeicleTracking({
+        this.code,
+        this.status,
+        this.mapData,
+    });
+
+    int? code;
+    bool? status;
+    MapData? mapData;
+    
+    factory GoogleMapVeicleTracking.fromJson(Map<String, dynamic> json) => GoogleMapVeicleTracking(
+        code: json["code"],
+        status: json["status"],
+        mapData: json["mapData"] == null ? null : MapData.fromJson(json["mapData"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "code": code,
+        "status": status,
+        "mapData": mapData?.toJson(),
+    };
+}
+
+class MapData {
+    MapData({
+        this.tipperDetails,
+        this.excavatorDetails,
+    });
+
+    List<TipperDetail>? tipperDetails;
+    List<ExcavatorDetail>? excavatorDetails;
+
+    factory MapData.fromJson(Map<String, dynamic> json) => MapData(
+        tipperDetails: json["Tipper_details"] == null ? [] : List<TipperDetail>.from(json["Tipper_details"]!.map((x) => TipperDetail.fromJson(x))),
+        excavatorDetails: json["Excavator_details"] == null ? [] : List<ExcavatorDetail>.from(json["Excavator_details"]!.map((x) => ExcavatorDetail.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Tipper_details": tipperDetails == null ? [] : List<dynamic>.from(tipperDetails!.map((x) => x.toJson())),
+        "Excavator_details": excavatorDetails == null ? [] : List<dynamic>.from(excavatorDetails!.map((x) => x.toJson())),
+    };
+}
+
+class ExcavatorDetail {
+    ExcavatorDetail({
+        this.excavatorId,
+        this.trackTime,
+        this.latitude,
+        this.longitude,
+        this.latDirection,
+        this.longDirection,
+        this.locationId,
+        this.direction,
+        this.state,
+        this.tipperState,
+    });
+
+    String? excavatorId;
+    String? trackTime;
+    String? latitude;
+    String? longitude;
+    String? latDirection;
+    String? longDirection;
+    String? locationId;
+    String? direction;
+    String? state;
+    String? tipperState;
+
+    factory ExcavatorDetail.fromJson(Map<String, dynamic> json) => ExcavatorDetail(
+        excavatorId: json["excavator_id"],
+        trackTime: json["trackTime"],
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+        latDirection: json["lat_direction"],
+        longDirection: json["long_direction"],
+        locationId: json["location_id"],
+        direction: json["direction"],
+        state: json["state"],
+        tipperState: json["tipperState"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "excavator_id": excavatorId,
+        "trackTime": trackTime,
+        "latitude": latitude,
+        "longitude": longitude,
+        "lat_direction": latDirection,
+        "long_direction": longDirection,
+        "location_id": locationId,
+        "direction": direction,
+        "state": state,
+        "tipperState": tipperState,
+    };
+}
+
+class TipperDetail {
+    TipperDetail({
+        this.tipperId,
+        this.trackTime,
+        this.latitude,
+        this.longitude,
+        this.latDirection,
+        this.longDirection,
+        this.direction,
+        this.state,
+    });
+
+    String? tipperId;
+    String? trackTime;
+    String? latitude;
+    String? longitude;
+    String? latDirection;
+    String? longDirection;
+    String? direction;
+    String? state;
+
+    factory TipperDetail.fromJson(Map<String, dynamic> json) => TipperDetail(
+        tipperId: json["tipper_id"],
+        trackTime: json["trackTime"],
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+        latDirection: json["lat_direction"],
+        longDirection: json["long_direction"],
+        direction: json["direction"],
+        state: json["state"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "tipper_id": tipperId,
+        "trackTime": trackTime,
+        "latitude": latitude,
+        "longitude": longitude,
+        "lat_direction": latDirection,
+        "long_direction": longDirection,
+        "direction": direction,
+        "state": state,
+    };
+}
+
--- lib/work.dart
+++ lib/work.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names, unnecessary_statements, prefer_const_constructors, prefer_collection_literals, prefer_final_fields, prefer_interpolation_to_compose_strings, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
 
 import 'dart:async';
 import 'dart:ffi';
@@ -7,49 +7,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_widget/google_maps_widget.dart';
+import 'package:truckotruck/main.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class MarkerMover extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
-  }
+  _MarkerMoverState createState() => _MarkerMoverState();
 }
 
-const String googleMapApiKey = "AIzaSyDW_-Ds0EqmP3GSwQ2IHtxvLJHYRMozVi8";
+class _MarkerMoverState extends State<MarkerMover> {
 
-//=========================================
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   List<LatLng> latLen = [
+    
     LatLng(11.03881588126287, 77.01362222601809),
     LatLng(11.039767608001304, 77.02673828145427),
     LatLng(11.04372475461159, 77.04133435834277),
     LatLng(11.067874684255145, 77.0411905117804),
-  ];
-  List<LatLng> latLen2 = [
     LatLng(11.10234739871648, 77.1291879729305),
     LatLng(11.116073704143734, 77.11168288714075),
     LatLng(11.131302881976933, 77.13771928039014),
-    LatLng(11.13274684255445, 77.15109051178414),
+    LatLng(11.13274684255445, 77.15109051178414),    
+    LatLng(11.141302881976933, 77.16771928039014),
+    LatLng(11.14979684255445, 77.17109051178414),
   ];
 
-  List<LatLng> latLen3 = [
-    LatLng(11.20234739871648, 77.2291879729305),
-    LatLng(11.216073704143734, 77.21168288714075),
-    LatLng(11.231302881976933, 77.23771928039014),
-    LatLng(11.243274684255445, 77.25109051178414),
-  ];
-  List rotater=[70.0,80.0,120.0,190.0,220.0,22.0,150.0,160.0,300.0,310.0,100.0,40.0];
+  List rotater=[70.0,80.0,120.0,190.0,220.0,22.0,150.0,160.0,300.0,310.0];
   
   List markermerge= [];
   //================ Image encode=============
@@ -79,7 +60,7 @@ class _HomeState extends State<Home> {
   Map<PolylineId, Polyline> polylines = {}; 
   
   void _marker() async {
-markermerge=[...latLen,...latLen2,...latLen3];
+markermerge=[...latLen];
 
     final Uint8List Icon1 = await getImages('assets/e_load.png');
     final Uint8List Icon2 = await getImages('assets/t_unload.png');
@@ -102,12 +83,20 @@ markermerge=[...latLen,...latLen2,...latLen3];
 
 
 int markerIndex=0;
-
+void showInfoWindow(String markerId) async {
+  final GoogleMapController controller = await mapController!;
+  controller.showMarkerInfoWindow(MarkerId(markerId));
+}
+void hideInfoWindow(String markerId) async {
+   GoogleMapController controller = await mapController!;
+  controller.hideMarkerInfoWindow(MarkerId(markerId));
+}
   moveMarker()async{
     
     final Uint8List Icon2 = await getImages('assets/t_unload.png');
     markers={};      
        markers.add(Marker(
+        rotation: rotater[markerIndex],
         anchor: Offset(0.5, 0.5),
         markerId: MarkerId(markerIndex.toString()),
         position: markermerge[markerIndex],
@@ -117,7 +106,7 @@ int markerIndex=0;
           snippet: 'Marker ID : ' + markerIndex.toString(),
         ),
       ));
-
+showInfoWindow(markerIndex.toString());
       if(markerIndex!=markermerge.length){
           markerIndex++;
           Future.delayed(Duration(milliseconds: 1500), (){
@@ -133,18 +122,6 @@ int markerIndex=0;
       polylineId: const PolylineId('direction'),
       color: Colors.blue,
       points: latLen,
-      width: 5,
-    ));
-    _directionPolyline.add(Polyline(
-      polylineId: const PolylineId('direction2'),
-      color: Colors.black,
-      points: latLen2,
-      width: 5,
-    ));
-    _directionPolyline.add(Polyline(
-      polylineId: const PolylineId('direction3'),
-      color: Colors.deepOrange,
-      points: latLen3,
       width: 5,
     ));
     _marker();
@@ -179,6 +156,8 @@ int markerIndex=0;
           mapType: MapType.normal,
           onMapCreated: (controller) {
             setState(() {
+              
+showInfoWindow(markerIndex.toString());
               mapController = controller;
             });
           },
